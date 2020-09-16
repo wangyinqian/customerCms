dc29dea8e82e4f12065869ba1631e262c541a818
wangyinqian <wangwenxiao03@163.com>
2020-09-16 14:29:34 +0800

    master

---
+++ back-end/customercms/src/main/java/com/customercms/customercms/CustomercmsApplication.java
@@ -0,0 +1,13 @@
+package com.customercms.customercms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomercmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomercmsApplication.class, args);
+	}
+
+}
---
+++ back-end/customercms/src/test/java/com/customercms/customercms/CustomercmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customercms.customercms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomercmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
